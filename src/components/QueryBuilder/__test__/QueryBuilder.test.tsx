c1b15f8db1402588c4cf6528fc722b52ddbafe6f
Viraj Chavan <virajc@live.com>
2020-11-02 09:51:34 +0530

    Add more tests for QueryBuilder

--- src/components/QueryBuilder/__test__/QueryBuilder.test.tsx
+++ src/components/QueryBuilder/__test__/QueryBuilder.test.tsx
@@ -1,11 +1,9 @@
-import { render, getByTestId, queryByText } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import React from "react";
 import userEvent from '@testing-library/user-event'
 import QueryBuilder from "../QueryBuilder";
 import queryConfig from '../../../queryConfig/queryConfig';
 
-// - "correct operator list is renderd"
-// - "operator should changed based on lhs"
 // - "rhs should change based on lhs"
 
 test("it should have mandatory elements", () => {
@@ -33,7 +31,30 @@ test("remove-row-button click should remove a row", () => {
 });
 
 test("it should render correct LHS options", () => {
-    const { getByText, queryByText } = render(<QueryBuilder queryConfig={queryConfig} />);
+    const { getByText, queryAllByText } = render(<QueryBuilder queryConfig={queryConfig} />);
     userEvent.click(getByText("+ Add"));
-    expect(queryByText(queryConfig[Object.keys(queryConfig)[0]].label)).toBeInTheDocument();
+    userEvent.click(getByText(queryConfig[Object.keys(queryConfig)[0]].label));
+
+    Object.keys(queryConfig).forEach(key => {
+        expect(queryAllByText(queryConfig[key].label)[0]).toBeInTheDocument();
+    });
+});
+
+test("correct operator list is rendered based on LHS", () => {
+    const { getByText, queryAllByText } = render(<QueryBuilder queryConfig={queryConfig} />);
+    userEvent.click(getByText("+ Add"));
+    let rowConfig1 = queryConfig[Object.keys(queryConfig)[0]];
+    userEvent.click(queryAllByText(rowConfig1.operators[0].text)[0]);
+    rowConfig1.operators.forEach(operator => {
+        expect(queryAllByText(operator.text)[0]).toBeInTheDocument();
+    });
+
+    userEvent.click(queryAllByText(rowConfig1.label)[0]);
+    let rowConfig2 = queryConfig[Object.keys(queryConfig)[1]];
+    userEvent.click(queryAllByText(rowConfig2.label)[0]);
+
+    userEvent.click(queryAllByText(rowConfig2.operators[0].text)[0]);
+    rowConfig2.operators.forEach(operator => {
+        expect(queryAllByText(operator.text)[0]).toBeInTheDocument();
+    });
 });
